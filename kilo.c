802c7dbd0bb4b519be82663941487a78eb321922
Miklós Tusz <mdtusz@gmail.com>
2019-08-26 10:50:35 -0400

    Add loop and timeout for character input reading

--- kilo.c
+++ kilo.c
@@ -39,18 +39,29 @@ void enableRawMode() {
     // Set character size to 8 bits per byte.
     raw.c_cflag |= (CS8);
 
+    raw.c_cc[VMIN] = 0;
+
+    // Set max time before `read` will timeout and return 0.
+    raw.c_cc[VTIME] = 1;
+
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
 
 int main() {
     enableRawMode();
 
-    char c;
-    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+    while (1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
+        }
+
+        if (c == 'q') {
+            break;
         }
     }
 
