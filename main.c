f461592cb4eb84c6401f7ec8ae4c06ea3f0526af
Miklós Tusz <mdtusz@gmail.com>
2019-08-26 23:54:27 -0400

    Refactor key input handling

--- main.c
+++ main.c
@@ -61,27 +61,34 @@ void enableRawMode() {
     }
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) {
+            die("read");
+        }
+    }
+
+    return c;
+}
+
+void editorProcessKey() {
+    char c = editorReadKey();
+
+    switch (c) {
+        case CTRL_KEY('q'):
+            exit(0);
+            break;
+    }
+}
+
 int main() {
     enableRawMode();
 
     while (1) {
-        char c = '\0';
-
-        // If running on Cygwin, `read` calls will timeout with `-1` and set
-        // `errno == EAGAIN` instead of returning 0.
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) {
-            die("read");
-        }
-
-        if (iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d ('%c')\r\n", c, c);
-        }
-
-        if (c == CTRL_KEY('q')) {
-            break;
-        }
+        editorProcessKey();
     }
 
     return 0;
