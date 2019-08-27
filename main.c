6decefd8ce157d459af50a37ca882e567ee700de
Miklós Tusz <mdtusz@gmail.com>
2019-08-27 00:48:04 -0400

    Draw ~ on last line of screen

--- main.c
+++ main.c
@@ -131,7 +131,11 @@ int getWindowSize(int *rows, int *cols) {
 void editorDrawRows() {
     int y;
     for (y = 0; y < E.screenRows; y++) {
-        write(STDOUT_FILENO, "~\r\n", 3);
+        write(STDOUT_FILENO, "~", 1);
+
+        if (y < E.screenRows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
     }
 }
 
